9e5641b29f7a77d4f23af85bb23576a8e4308bbf
Abu Sayed Mhd Hussain <abusayedmhdhussain@Abus-MacBook-Air.local>
2026-05-08 18:26:49 +0530

    fixed mongodb uri

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 CORS(app)
 
 # MongoDB Connection
-client = MongoClient("mongodb+srv://abiway880:abiway123@cluster0.8qpkfjs.mongodb.net/")
+client = MongoClient("mongodb+srv://abiway880:abiway123@cluster0.8qpkfjs.mongodb.net/trendtube_ai?retryWrites=true&w=majority")
 
 db = client["trendtube_ai"]
 
